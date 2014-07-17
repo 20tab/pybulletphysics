1ec8ea33b01f528ecf8a9e4f3d1912719b4b1b34
Francesco Leacche <francescoleacche@gmail.com>
2014-07-17 12:03:22 +0200

    added get and setCollisionFlags

--- src/RigidBody.cpp
+++ src/RigidBody.cpp
@@ -7,8 +7,8 @@ static void
 RigidBody_dealloc(bulletphysics_RigidBodyObject* self)
 {
 	Py_XDECREF(self->dict);
-        delete(self->rigidBody);
-        self->ob_type->tp_free((PyObject*)self);
+    delete(self->rigidBody);
+    self->ob_type->tp_free((PyObject*)self);
 }
 
 static PyObject*
@@ -16,17 +16,17 @@ RigidBody_new(PyTypeObject *type, PyObject *args, PyObject *kwds)
 {
 	PyObject *py_rigidbodyCI = NULL;
 	if (!PyArg_ParseTuple(args, "O", &py_rigidbodyCI)) {
-        	return NULL;
-    	}
+    	return NULL;
+	}
 
-        bulletphysics_RigidBodyObject *self = (bulletphysics_RigidBodyObject *)type->tp_alloc(type, 0);
-        if (self != NULL) {
-		bulletphysics_RigidBodyConstructionInfoObject *rigidbodyCI = (bulletphysics_RigidBodyConstructionInfoObject *) py_rigidbodyCI;
-                self->rigidBody = new btRigidBody(*(rigidbodyCI->constructionInfo));
-		self->rigidBody->setUserPointer(self);
-		self->dict = NULL;
-        }
-        return (PyObject *)self;
+    bulletphysics_RigidBodyObject *self = (bulletphysics_RigidBodyObject *)type->tp_alloc(type, 0);
+    if (self != NULL) {
+        bulletphysics_RigidBodyConstructionInfoObject *rigidbodyCI = (bulletphysics_RigidBodyConstructionInfoObject *) py_rigidbodyCI;
+        self->rigidBody = new btRigidBody(*(rigidbodyCI->constructionInfo));
+        self->rigidBody->setUserPointer(self);
+        self->dict = NULL;
+    }
+    return (PyObject *)self;
 }
 
 PyTypeObject bulletphysics_RigidBodyType = {
@@ -56,126 +56,126 @@ PyTypeObject bulletphysics_RigidBodyType = {
 
 static PyObject *
 RigidBody_applyCentralForce(bulletphysics_RigidBodyObject *self, PyObject *args, PyObject *kwds) {
-        PyObject *py_vector3 = NULL;
-        if (!PyArg_ParseTuple(args, "O", &py_vector3)) {
-                return NULL;
-        }
+    PyObject *py_vector3 = NULL;
+    if (!PyArg_ParseTuple(args, "O", &py_vector3)) {
+        return NULL;
+    }
 
-        bulletphysics_Vector3Object *vector3 = (bulletphysics_Vector3Object *) py_vector3;
-        self->rigidBody->applyCentralForce(*(vector3->vector));
+    bulletphysics_Vector3Object *vector3 = (bulletphysics_Vector3Object *) py_vector3;
+    self->rigidBody->applyCentralForce(*(vector3->vector));
 
-        Py_INCREF(Py_None);
-        return Py_None;
+    Py_INCREF(Py_None);
+    return Py_None;
 }
 
 static PyObject *
 RigidBody_applyForce(bulletphysics_RigidBodyObject *self, PyObject *args, PyObject *kwds) {
-        PyObject *py_vector3 = NULL;
-        PyObject *py_vector3_pos = NULL;
-        if (!PyArg_ParseTuple(args, "OO", &py_vector3, &py_vector3_pos)) {
-                return NULL;
-        }
+    PyObject *py_vector3 = NULL;
+    PyObject *py_vector3_pos = NULL;
+    if (!PyArg_ParseTuple(args, "OO", &py_vector3, &py_vector3_pos)) {
+        return NULL;
+    }
 
-        bulletphysics_Vector3Object *vector3 = (bulletphysics_Vector3Object *) py_vector3;
-        bulletphysics_Vector3Object *vector3_pos = (bulletphysics_Vector3Object *) py_vector3_pos;
-        self->rigidBody->applyForce(*(vector3->vector), *(vector3_pos->vector));
+    bulletphysics_Vector3Object *vector3 = (bulletphysics_Vector3Object *) py_vector3;
+    bulletphysics_Vector3Object *vector3_pos = (bulletphysics_Vector3Object *) py_vector3_pos;
+    self->rigidBody->applyForce(*(vector3->vector), *(vector3_pos->vector));
 
-        Py_INCREF(Py_None);
-        return Py_None;
+    Py_INCREF(Py_None);
+    return Py_None;
 }
 
 static PyObject *
 RigidBody_applyCentralImpulse(bulletphysics_RigidBodyObject *self, PyObject *args, PyObject *kwds) {
-        PyObject *py_vector3 = NULL;
-        if (!PyArg_ParseTuple(args, "O", &py_vector3)) {
-                return NULL;
-        }
+    PyObject *py_vector3 = NULL;
+    if (!PyArg_ParseTuple(args, "O", &py_vector3)) {
+        return NULL;
+    }
 
-        bulletphysics_Vector3Object *vector3 = (bulletphysics_Vector3Object *) py_vector3;
-        self->rigidBody->applyCentralImpulse(*(vector3->vector));
+    bulletphysics_Vector3Object *vector3 = (bulletphysics_Vector3Object *) py_vector3;
+    self->rigidBody->applyCentralImpulse(*(vector3->vector));
 
-        Py_INCREF(Py_None);
-        return Py_None;
+    Py_INCREF(Py_None);
+    return Py_None;
 }
 
 static PyObject *
 RigidBody_applyTorque(bulletphysics_RigidBodyObject *self, PyObject *args, PyObject *kwds) {
-        PyObject *py_vector3 = NULL;
-        if (!PyArg_ParseTuple(args, "O", &py_vector3)) {
-                return NULL;
-        }       
-        
-        bulletphysics_Vector3Object *vector3 = (bulletphysics_Vector3Object *) py_vector3;
-        self->rigidBody->applyTorque(*(vector3->vector));
-        
-        Py_INCREF(Py_None);
-        return Py_None;
+    PyObject *py_vector3 = NULL;
+    if (!PyArg_ParseTuple(args, "O", &py_vector3)) {
+        return NULL;
+    }
+
+    bulletphysics_Vector3Object *vector3 = (bulletphysics_Vector3Object *) py_vector3;
+    self->rigidBody->applyTorque(*(vector3->vector));
+
+    Py_INCREF(Py_None);
+    return Py_None;
 }
 
 static PyObject *
 RigidBody_applyTorqueImpulse(bulletphysics_RigidBodyObject *self, PyObject *args, PyObject *kwds) {
-        PyObject *py_vector3 = NULL;
-        if (!PyArg_ParseTuple(args, "O", &py_vector3)) {
-                return NULL;
-        }
+    PyObject *py_vector3 = NULL;
+    if (!PyArg_ParseTuple(args, "O", &py_vector3)) {
+        return NULL;
+    }
 
-        bulletphysics_Vector3Object *vector3 = (bulletphysics_Vector3Object *) py_vector3;
-        self->rigidBody->applyTorqueImpulse(*(vector3->vector));
+    bulletphysics_Vector3Object *vector3 = (bulletphysics_Vector3Object *) py_vector3;
+    self->rigidBody->applyTorqueImpulse(*(vector3->vector));
 
-        Py_INCREF(Py_None);
-        return Py_None;
+    Py_INCREF(Py_None);
+    return Py_None;
 }
 
 static PyObject *
 RigidBody_applyImpulse(bulletphysics_RigidBodyObject *self, PyObject *args, PyObject *kwds) {
-        PyObject *py_vector3 = NULL;
-        PyObject *py_vector3_pos = NULL;
-        if (!PyArg_ParseTuple(args, "OO", &py_vector3, &py_vector3_pos)) {
-                return NULL;
-        }
+    PyObject *py_vector3 = NULL;
+    PyObject *py_vector3_pos = NULL;
+    if (!PyArg_ParseTuple(args, "OO", &py_vector3, &py_vector3_pos)) {
+        return NULL;
+    }
 
-        bulletphysics_Vector3Object *vector3 = (bulletphysics_Vector3Object *) py_vector3;
-        bulletphysics_Vector3Object *vector3_pos = (bulletphysics_Vector3Object *) py_vector3_pos;
-        self->rigidBody->applyImpulse(*(vector3->vector), *(vector3_pos->vector));
+    bulletphysics_Vector3Object *vector3 = (bulletphysics_Vector3Object *) py_vector3;
+    bulletphysics_Vector3Object *vector3_pos = (bulletphysics_Vector3Object *) py_vector3_pos;
+    self->rigidBody->applyImpulse(*(vector3->vector), *(vector3_pos->vector));
 
-        Py_INCREF(Py_None);
-        return Py_None;
+    Py_INCREF(Py_None);
+    return Py_None;
 }
 
 static PyObject *
 RigidBody_setLinearVelocity(bulletphysics_RigidBodyObject *self, PyObject *args, PyObject *kwds) {
-        PyObject *py_vector3 = NULL;
-        if (!PyArg_ParseTuple(args, "O", &py_vector3)) {
-                return NULL;
-        }
+    PyObject *py_vector3 = NULL;
+    if (!PyArg_ParseTuple(args, "O", &py_vector3)) {
+        return NULL;
+    }
 
-        bulletphysics_Vector3Object *vector3 = (bulletphysics_Vector3Object *) py_vector3;
-        self->rigidBody->setLinearVelocity(*(vector3->vector));
+    bulletphysics_Vector3Object *vector3 = (bulletphysics_Vector3Object *) py_vector3;
+    self->rigidBody->setLinearVelocity(*(vector3->vector));
 
-        Py_INCREF(Py_None);
-        return Py_None;
+    Py_INCREF(Py_None);
+    return Py_None;
 }
 
 static PyObject *
 RigidBody_setAngularVelocity(bulletphysics_RigidBodyObject *self, PyObject *args, PyObject *kwds) {
-        PyObject *py_vector3 = NULL;
-        if (!PyArg_ParseTuple(args, "O", &py_vector3)) {
-                return NULL;
-        }
+    PyObject *py_vector3 = NULL;
+    if (!PyArg_ParseTuple(args, "O", &py_vector3)) {
+        return NULL;
+    }
 
-        bulletphysics_Vector3Object *vector3 = (bulletphysics_Vector3Object *) py_vector3;
-        self->rigidBody->setAngularVelocity(*(vector3->vector));
+    bulletphysics_Vector3Object *vector3 = (bulletphysics_Vector3Object *) py_vector3;
+    self->rigidBody->setAngularVelocity(*(vector3->vector));
 
-        Py_INCREF(Py_None);
-        return Py_None;
+    Py_INCREF(Py_None);
+    return Py_None;
 }
 
 static PyObject *
 RigidBody_activate(bulletphysics_RigidBodyObject *self, PyObject *args, PyObject *kwds) {
-        PyObject *py_bool = NULL;
-        if (!PyArg_ParseTuple(args, "O", &py_bool)) {
-                return NULL;
-        }
+    PyObject *py_bool = NULL;
+    if (!PyArg_ParseTuple(args, "O", &py_bool)) {
+        return NULL;
+    }
 
 	if (py_bool == Py_False || py_bool == Py_None) {
 		self->rigidBody->activate(false);
@@ -184,49 +184,62 @@ RigidBody_activate(bulletphysics_RigidBodyObject *self, PyObject *args, PyObject
 		self->rigidBody->activate(true);
 	}
 
-        Py_INCREF(Py_None);
-        return Py_None;
+    Py_INCREF(Py_None);
+    return Py_None;
 }
 
 static PyObject *
 RigidBody_setCenterOfMassTransform(bulletphysics_RigidBodyObject *self, PyObject *args, PyObject *kwds) {
 	bulletphysics_TransformObject *py_transform = NULL;
-        if (!PyArg_ParseTuple(args, "O", &py_transform)) {
-                return NULL;
-        }
+    if (!PyArg_ParseTuple(args, "O", &py_transform)) {
+        return NULL;
+    }
 	pybulletphysics_checktype(py_transform, Transform);
 	self->rigidBody->setCenterOfMassTransform(*(py_transform->transform));
-        Py_INCREF(Py_None);
-        return Py_None;
+    Py_INCREF(Py_None);
+    return Py_None;
 }
 
 static PyObject *
 RigidBody_setActivationState(bulletphysics_RigidBodyObject *self, PyObject *args, PyObject *kwds) {
 	int state = 0;
-        if (!PyArg_ParseTuple(args, "i", &state)) {
-                return NULL;
-        }
-        self->rigidBody->setActivationState(state);
-        Py_INCREF(Py_None);
-        return Py_None;
+    if (!PyArg_ParseTuple(args, "i", &state)) {
+        return NULL;
+    }
+    self->rigidBody->setActivationState(state);
+    Py_INCREF(Py_None);
+    return Py_None;
 }
 
 static PyObject *
 RigidBody_getActivationState(bulletphysics_RigidBodyObject *self, PyObject *args, PyObject *kwds) {
-	PyInt_FromLong(self->rigidBody->getActivationState());
+    return PyInt_FromLong(self->rigidBody->getActivationState());
 }
-
-
-
 
 static PyObject *
 RigidBody_getOrientation(bulletphysics_RigidBodyObject *self, PyObject *args, PyObject *kwds) {
-        return new_pyquaternion_from_quaternion(self->rigidBody->getOrientation());
+    return new_pyquaternion_from_quaternion(self->rigidBody->getOrientation());
 }
 
 static PyObject *
 RigidBody_getLinearVelocity(bulletphysics_RigidBodyObject *self, PyObject *args, PyObject *kwds) {
-        return new_pyvector3_from_vector(self->rigidBody->getLinearVelocity());
+    return new_pyvector3_from_vector(self->rigidBody->getLinearVelocity());
+}
+
+static PyObject *
+RigidBody_getCollisionFlags(bulletphysics_RigidBodyObject *self, PyObject *args, PyObject *kwds) {
+    return PyInt_FromLong(self->rigidBody->getCollisionFlags());
+}
+
+static PyObject *
+RigidBody_setCollisionFlags(bulletphysics_RigidBodyObject *self, PyObject *args, PyObject *kwds) {
+    int flags = 0;
+    if (!PyArg_ParseTuple(args, "i", &flags)) {
+        return NULL;
+    }
+    self->rigidBody->setCollisionFlags(flags);
+    Py_INCREF(Py_None);
+    return Py_None;
 }
 
 static PyMethodDef RigidBody_methods[] = {
@@ -244,6 +257,8 @@ static PyMethodDef RigidBody_methods[] = {
     {"getLinearVelocity", (PyCFunction)RigidBody_getLinearVelocity, METH_VARARGS, NULL },
     {"setActivationState", (PyCFunction)RigidBody_setActivationState, METH_VARARGS, NULL },
     {"getActivationState", (PyCFunction)RigidBody_getActivationState, METH_VARARGS, NULL },
+    {"getCollisionFlags", (PyCFunction)RigidBody_getCollisionFlags, METH_VARARGS, NULL },
+    {"setCollisionFlags", (PyCFunction)RigidBody_setCollisionFlags, METH_VARARGS, NULL },
     {NULL, NULL, 0, NULL}
 };
 
@@ -252,10 +267,10 @@ void pybulletphysics_add_RigidBody(PyObject *module) {
 	bulletphysics_RigidBodyType.tp_dictoffset = offsetof(bulletphysics_RigidBodyObject, dict);
 	bulletphysics_RigidBodyType.tp_new = RigidBody_new;
 
-        if (PyType_Ready(&bulletphysics_RigidBodyType) < 0)
-                return;	
+    if (PyType_Ready(&bulletphysics_RigidBodyType) < 0)
+        return;
 
 	Py_INCREF(&bulletphysics_RigidBodyType);
 
-        PyModule_AddObject(module, "RigidBody", (PyObject *)&bulletphysics_RigidBodyType);
+    PyModule_AddObject(module, "RigidBody", (PyObject *)&bulletphysics_RigidBodyType);
 }
